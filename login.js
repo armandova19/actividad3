d4ebcf6dc68ab4de9ac85eeb409aaee0fb1cfe30
armandova19 <jvazquez47@ucol.mx>
2021-05-14 13:56:56 -0500

    actividad 3

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-//Ahora podemos conectarnos a nuestra base de datos con el siguiente código:
+
 var connection = mysql.createConnection({
 host     : 'localhost',
 user     : 'root',
@@ -15,52 +15,52 @@ database : 'nodelogin'
 //Express es lo que usaremos para nuestras aplicaciones web, esto incluye paquetes útiles en el desarrollo web, como sesiones y manejo de solicitudes HTTP, para inicializarlo podemos hacer:
 var app = express();
 
-//Ahora necesitamos informarle a Express que usaremos algunos de sus paquetes:
+
 app.use(session({
 secret: 'secret',
 resave: true,
 saveUninitialized: true
 }));
 
-//Asegúrese de cambiar el código secreto de las sesiones, el paquete de sesiones es lo que usaremos para determinar si el usuario ha iniciado sesión, el paquete bodyParser extraerá los datos del formulario de nuestro archivo login.html.
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-//Ahora necesitamos mostrar nuestro archivo login.html al cliente:
+
 app.get('/', function(request, response) {
-response.sendFile(path.join(__dirname + '/login.html')); //Cuando el cliente se conecta al servidor, se mostrará la página de inicio de sesión, el servidor enviará el archivo login.html.
+response.sendFile(path.join(__dirname + '/login.html')); 
 });
 
 
 app.post('/auth', function(request, response) {
-    var username = request.body.username; //Obtenemos el nombre de usuario.
-    var password = request.body.password; //Obtenemos la contraseña. 
+    var username = request.body.username; 
+    var password = request.body.password; 
 
-    if (username && password) { // Verificamos que no estén vacios.
-        //Sino están vacios los campos hacemos la consulta para verificar el usuario y contraseña.
+    if (username && password) { 
+        
         connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-        if (results.length > 0) { // El result es más de 1, es que si se encuentra correcto.
-            request.session.loggedin = true; // asignamos a loggedin como TRUE
+        if (results.length > 0) { 
+            request.session.loggedin = true; 
             request.session.username = username; // Asignamos el nombre de usuario a una variable de sesión.
-            response.redirect('/home'); //Redireccionamos a /home
+            response.redirect('/home'); 
         } else {
-            response.send('Usuarios y/o contraseña incorrectos!'); //En dado caso que el result de 0, es que no encontro coincidencias en la consulta de usuario y contraseña; mandamos mensaje.
+            response.send('Usuarios y/o contraseña incorrectos!'); 
         }
-        response.end(); // Terminamos el proceso
+        response.end(); 
         });
     } else {
-        response.send('Ingresa usuario y contraseña!'); // Si se encuentran vacios los campos mandamos mensaje.
-        response.end(); // Terminamos el proceso.
+        response.send('Ingresa usuario y contraseña!'); 
+        response.end(); 
     }
 });
 
 app.get('/home', function(request, response) {
-if (request.session.loggedin) { // Verificamos la variable de sesión loggedin sea TRUE
-response.send('Bienvenido de nuevo, ' + request.session.username + '! <br><br> <a href="/logout" class="btn btn-success">Cerrar sesión</a>'); // Si es TRUE mostramos mensaje con el nombre de usuario de la sesión.
+if (request.session.loggedin) { 
+response.send('Bienvenido de nuevo, ' + request.session.username + '! <br><br> <a href="/logout" class="btn btn-success">Cerrar sesión</a>'); 
 } else {
-response.send('Iniciar sesión de nuevo, por favor!'); // Si loggedin es FALSO solicitamos que inicie sesión de nuevo.
+response.send('Iniciar sesión de nuevo, por favor!'); 
 }
-response.end(); // Terminamos proceso.
+response.end(); 
 });
 
 // Cerrar sesión 
@@ -69,7 +69,7 @@ app.get('/logout', function (request, response) {
   response.send('Sesión terminada correctamente <br><br> <a href="/" class="btn btn-success">Inicar sesión de nuevo.</a>');
 });
 
-//Nuestra aplicación web necesita escuchar en un puerto, para propósitos de prueba usaremos el puerto 3000:
+
 app.listen(3000, function(){
 console.log('Puesto en marcha el server en puerto 3000');
 });
